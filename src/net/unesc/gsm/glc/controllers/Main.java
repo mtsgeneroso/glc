331966464c0fcffd5107f811a98eb5438394e12e
Mateus Generoso <mtsgeneroso@gmail.com>
2017-06-07 21:22:04 -0300

    Revisão de código. Definição dos campos do internal frame para adicionar produções.

--- src/net/unesc/gsm/glc/controllers/Main.java
+++ src/net/unesc/gsm/glc/controllers/Main.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.unesc.gsm.glc.controllers;
 
-/**
- *
- * @author Mateus Silvano
- */
 public class Main {
     
 }
